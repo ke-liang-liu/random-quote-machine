a16a232bea629443a31d19ecefb35dfdc3a7bd0f
Keliang Liu <keliangliu1@gmail.com>
2019-12-25 21:44:50 +1100

    fetch quote json data; show quote and author

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quoteIndex, setQuoteIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+      setQuotes(result.data.quotes);
+      // setQuoteIndex(random(0, quotes.length - 1));
+    }
+    fetchData();
+    /*
+        Line 15/22  React Hook useEffect has a missing dependency: 'quotes.length'. Either include it or remove the dependency array. 
+        You can also replace multiple useState variables with useReducer if 'setQuoteIndex' needs the current value of 'quotes.length' 
+    */
+  }, [])
+  /**
+   * set the first random index when page loads
+   */
+  useEffect(() => {
+    if (quotes.length > 0) {
+      setQuoteIndex(Math.floor(Math.random() * quotes.length));
+    }
+  }, [quotes])
+
+  /**
+   * get Quote object from states
+   */
+  function getQuote() {
+    if (!quotes.length || !Number.isInteger(quoteIndex)) {
+      return undefined;
+    }
+    return quotes[quoteIndex];
+  }
+
+  /**
+   * generate a random integer as Index of quotes
+   */
+  function genRandomQuoteIndex() {
+    return Math.floor(Math.random() * quotes.length);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id='quote-box'>
+      {getQuote() ? `"${getQuote().quote}" - ${getQuote().author}` : <p>{quoteIndex}</p>}
     </div>
   );
 }
