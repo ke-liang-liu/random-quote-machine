0cf681bdb368b2f6d9e7574b440be9abb27a2b48
Keliang Liu <keliangliu1@gmail.com>
2019-12-28 14:40:34 +1100

    Updates

---
+++ precache-manifest.16065918c94154e47dae5537d4812f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04355309804fad6fc25f748ece6f0a1",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "7952e7b3aa95149373d3",
+    "url": "/random-quote-machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6819a319878016b04134",
+    "url": "/random-quote-machine/static/js/2.96591ef1.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/random-quote-machine/static/js/2.96591ef1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7952e7b3aa95149373d3",
+    "url": "/random-quote-machine/static/js/main.e6f5efd5.chunk.js"
+  },
+  {
+    "revision": "e0c7647b3ff770dd409c",
+    "url": "/random-quote-machine/static/js/runtime-main.80a202ed.js"
+  }
+]);
